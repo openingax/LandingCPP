90b46c79b8c5e4abffbd283d30ed70031abff991
xieliying <xiely8638@outlook.com>
2022-08-26 15:55:53 +0800

    initial commit

---
+++ LandingCPP/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  LandingCPP
+//
+//  Created by 谢立颖 on 2022/8/26.
+//
+
+#include <iostream>
+
+//using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!" << std::endl;
+    
+    auto miles = 100;
+    printf("%d", miles);
+    return 0;
+}
